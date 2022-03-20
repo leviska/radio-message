f4b741e7d5f6dc5a0a48d8769f501f5233200d95
Iskandarov Lev <iskandarov11@inbox.ru>
2022-03-20 05:00:53 +0300

    small refactor

--- src/actors/gossip.rs
+++ src/actors/gossip.rs
@@ -11,60 +11,52 @@ pub async fn gossip_actor(my_id: u32, mut ctx: Context<GossipMessage>) {
     log::info!("worker {} started", my_id);
     let mut history = HashMap::<u32, GossipMessage>::default();
     let mut last_broadcast = 0;
-    loop {
-        let event = ctx.read_for(10).await;
+    while let Ok(event) = ctx.read_for(10).await {
         match event {
-            Ok(m) => {
-                match m {
-                    Some(m) => match m.data {
-                        MessageType::Request(m) => {
-                            let gm = GossipMessage::Request(m);
-                            history.entry(m.id).or_insert(gm.clone());
-                            ctx.send(MessageType::Comm(gm));
-                        }
-                        MessageType::Comm(m) => match m {
-                            GossipMessage::Request(m) => {
-                                if m.to == my_id {
-                                    ctx.send(MessageType::Request(m));
-                                    history.entry(m.id).or_insert(GossipMessage::Ack(m.id));
-                                }
-                                // if history[id] == Ack
-                                if history
-                                    .get(&m.id)
-                                    .map(|x| matches!(x, GossipMessage::Ack(_)))
-                                    .unwrap_or_default()
-                                {
-                                    ctx.send(MessageType::Comm(GossipMessage::Ack(m.id)));
-                                } else {
-                                    ctx.send(MessageType::Comm(GossipMessage::Request(m)));
-                                }
-                            }
-                            GossipMessage::Ack(id) => {
-                                // if history[id] != Ack
-                                if !history
-                                    .get(&id)
-                                    .map(|x| matches!(x, GossipMessage::Ack(_)))
-                                    .unwrap_or_default()
-                                {
-                                    ctx.send(MessageType::Comm(GossipMessage::Ack(id)));
-                                }
-                            }
-                        },
-                    },
-                    None => {}
+            Some(m) => match m.data {
+                MessageType::Request(m) => {
+                    let gm = GossipMessage::Request(m);
+                    history.entry(m.id).or_insert(gm.clone());
+                    ctx.send(MessageType::Comm(gm));
                 }
-                if ctx.current_step() - last_broadcast > 100 {
-                    for (_, m) in history.iter() {
-                        if matches!(m, GossipMessage::Request(_)) {
-                            ctx.send(MessageType::Comm(m.clone()));
+                MessageType::Comm(m) => match m {
+                    GossipMessage::Request(m) => {
+                        if m.to == my_id {
+                            ctx.send(MessageType::Request(m));
+                            history.entry(m.id).or_insert(GossipMessage::Ack(m.id));
+                        }
+                        // if history[id] == Ack
+                        if history
+                            .get(&m.id)
+                            .map(|x| matches!(x, GossipMessage::Ack(_)))
+                            .unwrap_or_default()
+                        {
+                            ctx.send(MessageType::Comm(GossipMessage::Ack(m.id)));
+                        } else {
+                            ctx.send(MessageType::Comm(GossipMessage::Request(m)));
                         }
                     }
-                    last_broadcast = ctx.current_step();
+                    GossipMessage::Ack(id) => {
+                        // if history[id] != Ack
+                        if !history
+                            .get(&id)
+                            .map(|x| matches!(x, GossipMessage::Ack(_)))
+                            .unwrap_or_default()
+                        {
+                            ctx.send(MessageType::Comm(GossipMessage::Ack(id)));
+                        }
+                    }
+                },
+            },
+            None => {}
+        }
+        if ctx.current_step() - last_broadcast > 100 {
+            for (_, m) in history.iter() {
+                if matches!(m, GossipMessage::Request(_)) {
+                    ctx.send(MessageType::Comm(m.clone()));
                 }
             }
-            Err(_) => {
-                break;
-            }
+            last_broadcast = ctx.current_step();
         }
     }
     log::info!("worker {} stopped", my_id);
